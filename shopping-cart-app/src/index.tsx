baaf62bdfdc3422937574cfe5850bb5598062a1a
Lxb <905637031@qq.com>
2021-06-12 23:51:31 +0800

    feat: 样式配置

--- shopping-cart-app/src/index.tsx
+++ shopping-cart-app/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './assets/js/common'
+import './assets/css/resets.css'
+import './assets/css/border.css'
+import './assets/css/iconfont.css'
 
 ReactDOM.render(
   <React.StrictMode>
