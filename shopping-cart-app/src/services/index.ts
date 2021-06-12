50e88c6c62f3eaa994fba8735060db6521be4e9c
Lxb <905637031@qq.com>
2021-06-12 23:52:12 +0800

    feat: 请求封装

---
+++ shopping-cart-app/src/services/index.ts
@@ -0,0 +1,13 @@
+import axios from '../libs/http'
+
+function getPhones<T>(): Promise<T> {
+    return axios.get<T, T>('/phones').then((data: T) => {
+        return data
+    }).catch((err) => {
+        throw new Error('Request failed ' + err)
+    })
+}
+
+export {
+    getPhones
+}
